b3938b0d8825a423be85cf2aeb2ec3dd7fceb271
victor <vic.kloeppel@gmail.com>
2022-11-29 14:03:03 +0100

    app only

--- api/settings.ts
+++
@@ -1,7 +0,0 @@
-export default
-{
-    port: 3000,
-    host: "localhost",
-    dbPort: 3306,
-    dbHost: "localhost"
-}
--- api/src/logger.ts
+++
@@ -1,7 +0,0 @@
-import logger from "pino";
-import dayjs from "dayjs";
-import PinoPretty from "pino-pretty"
-
-const log = logger(PinoPretty());
-
-export default log;
